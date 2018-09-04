7cc1c5dea47f6d1bdf9b06aaa35a234504de84e3
able8 <able8@me.com>
2018-09-04 16:08:00 +0800

    13.web 服务器 part5 路由

--- README.md
+++ README.md
@@ -457,3 +457,36 @@ var server = require('./server')
 
 server.startServer()
 ```
+
+## 13.web 服务器 part5 路由
+
+- `console.dir(xx)` 查看对象的所有属性和方法
+- `req.url` 请求中包含url等属性
+
+```js
+unction startServer() {
+    var onRequest = function (req, res) {
+        console.log('request received ' + req.url)
+
+        if (req.url === '/' || req.url === '/home') {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(res)
+        } else if (req.url === '/review') {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            fs.createReadStream(__dirname + '/review.html', 'utf8').pipe(res)
+        } else if (req.url === '/api/v1/records') {
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            var jsonObj = {
+                name: 'able'
+            }
+            res.end(JSON.stringify(jsonObj))
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            fs.createReadStream(__dirname + '/404.html', 'utf8').pipe(res)
+        }
+    }
+    var server = http.createServer(onRequest)
+    server.listen(3000)
+    console.log('server started on http://127.0.0.1:3000')
+}
+```
--- server.js
+++ server.js
@@ -3,11 +3,24 @@ var fs = require('fs')
 
 function startServer() {
     var onRequest = function (req, res) {
-        console.log('request received')
-        res.writeHead(200, { 'Content-Type': 'text/html' })
+        console.log('request received ' + req.url)
 
-        var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
-        myReadStream.pipe(res)
+        if (req.url === '/' || req.url === '/home') {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(res)
+        } else if (req.url === '/review') {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            fs.createReadStream(__dirname + '/review.html', 'utf8').pipe(res)
+        } else if (req.url === '/api/v1/records') {
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            var jsonObj = {
+                name: 'able'
+            }
+            res.end(JSON.stringify(jsonObj))
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            fs.createReadStream(__dirname + '/404.html', 'utf8').pipe(res)
+        }
     }
     var server = http.createServer(onRequest)
     server.listen(3000)
